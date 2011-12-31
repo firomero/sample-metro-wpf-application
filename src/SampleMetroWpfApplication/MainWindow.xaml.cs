a3b44ee7046d373c1f61601041e72c54ea58948f
Alastair Pitts <Alastair Pitts@localhost>
2011-12-31 17:23:32 +1100

    - Initial code commit of sample Metro WPF application

---
+++ src/SampleMetroWpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace SampleMetroWpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Thickness MainBorderMaximizedPadding = new Thickness(13);
+        private static readonly Thickness MainBorderNormalPadding = new Thickness(5);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            bdrRoot.Padding = WindowState == WindowState.Maximized
+                                  ? MainBorderMaximizedPadding
+                                  : MainBorderNormalPadding;
+        }
+    }
+}
